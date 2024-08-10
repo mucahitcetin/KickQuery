36961c318258b34c644299ccc4602ff58bfe9271
mucahitcetin <mucahitcetin01@gmail.com>
2024-08-10 23:18:51 +0300

    heropage and filter added

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,5 +1,18 @@
+import Color from "./Color";
+import Gender from "./Gender";
+import Price from "./Price";
+import Size from "./Size";
+
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className="max-lg:max-w-lg max-lg:mx-auto max-lg:h-[80vh] ">
+      <h2 className="text-xl font-semibold max-lg:bg-white max-lg:flex max-lg:justify-between max-lg:p-4 rounded-t-md"></h2>
+      <Size />
+      <Color />
+      <Gender />
+      <Price />
+    </div>
+  );
 };
 
 export default Filter;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,22 @@
+import Filter from "../../components/Filter";
+import Hero from "./Hero";
+import List from "./List";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+
+      <h1 className="text-[20px] md:text-[36px] font-semibold my-[32px]">
+        Sana Uygun Seçenekler
+      </h1>
+
+      <div>
+        <Filter />
+        <List />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
